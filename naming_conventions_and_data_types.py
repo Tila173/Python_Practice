49381391372bf3a944f109c96ded53b9229ed9c4
Tila Muhammad <w4s.tila@gmail.com>
2026-01-09 07:47:21 +0500

    Add practice file for naming conventions and data types

---
+++ naming_conventions_and_data_types.py
@@ -0,0 +1,37 @@
+# Naming Convention
+
+#Pascal Case
+TilaMuhammad = "Hello, World!"
+print(TilaMuhammad)
+
+#Camel Case
+tilaMuhammad = "Hello, World!"      
+print(tilaMuhammad)
+
+#Snake Case, Recommended   
+tila_muhammad = "Hello, World!" 
+print(tila_muhammad)
+
+# Data Types in Python
+# Numeric Types
+integer_num = 10          # Integer
+float_num = 10.5          # Float
+complex_num = 1j          # Complex number
+print(type(integer_num))
+print(type(float_num))  
+print(type(complex_num))        
+
+# Boolean Type
+is_true = True        # Boolean
+is_false = False      # Boolean
+print(type(is_true))
+print(type(is_false))
+
+# String Type
+string_var = "Hello, Python!"   # String
+print(type(string_var))
+
+# Character Type
+a = 'A'
+print(ord(a))
+print(chr(65))
